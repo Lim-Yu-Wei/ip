846a335783229bffe2b33abe5da633657d0b5e43
Synesty <limyuwei04@gmail.com>
2026-08-26 22:26:27 +0800

    Use a Task class to represent tasks

--- src/main/java/YuWei.java
+++ src/main/java/YuWei.java
@@ -1,30 +1,17 @@
 import java.util.Scanner;
 
-/**
- * A chatbot that keeps track of a list of tasks entered by the user.
- *
- * <p>This is the Level-3 increment: tasks can be added, listed, and marked
- * as done or not done. The chatbot runs until the user enters "bye".
- */
 public class YuWei {
-    /** The name the chatbot introduces itself with. */
     private static final String BOT_NAME = "YuWei";
-
-    /** The horizontal rule printed around each block of output. */
     private static final String DIVIDER =
             "    ____________________________________________________________";
-
-    /** The maximum number of tasks that can be stored. */
     private static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        String[] tasks = new String[MAX_TASKS];
-        boolean[] completed = new boolean[MAX_TASKS];
+        Task[] tasks = new Task[MAX_TASKS];
         int index = 0;
 
-        // Greet the user on startup.
         System.out.println(DIVIDER);
         System.out.println("     Hello! I'm " + BOT_NAME);
         System.out.println("     What can I do for you?");
@@ -41,15 +28,17 @@ public class YuWei {
                     System.out.println("     Here are the tasks in your list:");
                     for (int i = 0; i < index; i++) {
                         System.out.println("     " + (i + 1) + ".["
-                                + (completed[i] ? "X" : " ") + "] " + tasks[i]);
+                                + tasks[i].getStatusIcon() + "] "
+                                + tasks[i].getDescription());
                     }
                 }
                 case "mark" -> {
                     int taskNumber = Integer.parseInt(parts[1]) - 1;
                     if (taskNumber >= 0 && taskNumber < index) {
-                        completed[taskNumber] = true;
+                        tasks[taskNumber].markAsDone();
                         System.out.println("     Nice! I've marked this task as done:");
-                        System.out.println("       [X] " + tasks[taskNumber]);
+                        System.out.println("       [" + tasks[taskNumber].getStatusIcon()
+                                + "] " + tasks[taskNumber].getDescription());
                     } else {
                         System.out.println("     Sorry, that task does not exist!");
                     }
@@ -57,15 +46,16 @@ public class YuWei {
                 case "unmark" -> {
                     int taskNumber = Integer.parseInt(parts[1]) - 1;
                     if (taskNumber >= 0 && taskNumber < index) {
-                        completed[taskNumber] = false;
+                        tasks[taskNumber].markAsNotDone();
                         System.out.println("     OK, I've marked this task as not done yet:");
-                        System.out.println("       [ ] " + tasks[taskNumber]);
+                        System.out.println("       [" + tasks[taskNumber].getStatusIcon()
+                                + "] " + tasks[taskNumber].getDescription());
                     } else {
                         System.out.println("     Sorry, that task does not exist!");
                     }
                 }
                 default -> {
-                    tasks[index++] = line;
+                    tasks[index++] = new Task(line);
                     System.out.println("     added: " + line);
                 }
             }
@@ -75,7 +65,6 @@ public class YuWei {
             line = in.nextLine();
         }
 
-        // Say goodbye before the program ends.
         System.out.println(DIVIDER);
         System.out.println("     Bye. Hope to see you again soon!");
         System.out.println(DIVIDER);
