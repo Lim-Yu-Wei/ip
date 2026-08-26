ac9a985426e8bc14afa9eb183e7237e5f9371dff
Synesty <limyuwei04@gmail.com>
2026-08-26 22:13:32 +0800

    Add ability to mark tasks as done and undone

--- src/main/java/YuWei.java
+++ src/main/java/YuWei.java
@@ -1,63 +1,83 @@
 import java.util.Scanner;
 
-
-
+/**
+ * A chatbot that keeps track of a list of tasks entered by the user.
+ *
+ * <p>This is the Level-3 increment: tasks can be added, listed, and marked
+ * as done or not done. The chatbot runs until the user enters "bye".
+ */
 public class YuWei {
     /** The name the chatbot introduces itself with. */
     private static final String BOT_NAME = "YuWei";
 
+    /** The horizontal rule printed around each block of output. */
+    private static final String DIVIDER =
+            "    ____________________________________________________________";
+
+    /** The maximum number of tasks that can be stored. */
+    private static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello! I'm " + BOT_NAME);
-        System.out.println("What can I do for you?");
-        String line = in.nextLine();
-        String[] tasks = new String[100];
-        boolean[] completed = new boolean[100];
+
+        String[] tasks = new String[MAX_TASKS];
+        boolean[] completed = new boolean[MAX_TASKS];
         int index = 0;
 
+        // Greet the user on startup.
+        System.out.println(DIVIDER);
+        System.out.println("     Hello! I'm " + BOT_NAME);
+        System.out.println("     What can I do for you?");
+        System.out.println(DIVIDER);
+        System.out.println();
+
+        String line = in.nextLine();
         while (!line.equals("bye")) {
             String[] parts = line.split(" ");
+            System.out.println(DIVIDER);
+
             switch (parts[0]) {
-
                 case "list" -> {
-                    for(int i = 0; i < index; i++){
-                        System.out.println((i+1) + "." + "[" + (completed[i] ? "X" : " ") + "] " + tasks[i]);
+                    System.out.println("     Here are the tasks in your list:");
+                    for (int i = 0; i < index; i++) {
+                        System.out.println("     " + (i + 1) + ".["
+                                + (completed[i] ? "X" : " ") + "] " + tasks[i]);
                     }
-
                 }
                 case "mark" -> {
                     int taskNumber = Integer.parseInt(parts[1]) - 1;
-                    if (taskNumber < index && !completed[taskNumber]) {
+                    if (taskNumber >= 0 && taskNumber < index) {
                         completed[taskNumber] = true;
-                        System.out.println("Nice! I've marked this task as done:\n   [X] " + tasks[taskNumber]);
-                    }else{
-                        System.out.println("Sorry, this task is either completed or doesnt exist!");
+                        System.out.println("     Nice! I've marked this task as done:");
+                        System.out.println("       [X] " + tasks[taskNumber]);
+                    } else {
+                        System.out.println("     Sorry, that task does not exist!");
                     }
                 }
-
                 case "unmark" -> {
                     int taskNumber = Integer.parseInt(parts[1]) - 1;
-                    if(taskNumber < index && completed[taskNumber]){
-                        completed[taskNumber+1] = false;
-                        System.out.println("OK, I've marked this task as not done yet:\n   [ ] " +  tasks[taskNumber]);
-                    }else{
-                        System.out.println("Sorry, this task is either still outstanding or doesnt exist!");
+                    if (taskNumber >= 0 && taskNumber < index) {
+                        completed[taskNumber] = false;
+                        System.out.println("     OK, I've marked this task as not done yet:");
+                        System.out.println("       [ ] " + tasks[taskNumber]);
+                    } else {
+                        System.out.println("     Sorry, that task does not exist!");
                     }
-
                 }
                 default -> {
-                    System.out.println("added: " + line);
-
                     tasks[index++] = line;
-
+                    System.out.println("     added: " + line);
                 }
-
             }
-            line = in.nextLine();
 
+            System.out.println(DIVIDER);
+            System.out.println();
+            line = in.nextLine();
         }
+
         // Say goodbye before the program ends.
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
     }
 }
